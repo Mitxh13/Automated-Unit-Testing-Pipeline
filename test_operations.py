a6380accc20a135bc34c9691a8ffe0a68310562b
TahirSiddique092 <mohammedsiddique729@gmail.com>
2026-09-09 11:56:23 +0530

    feat: added operations and test operations file

---
+++ test_operations.py
@@ -0,0 +1,13 @@
+from operations import add, multiply, power
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+    assert multiply(0, 5) == 0
+
+def test_power():
+    assert power(2, 3) == 8
+    assert power(5, 0) == 1
